ab3f57c351e42123ab27a2b4d5900f117d23f053
Joan Marie Verba <verba001@umn.edu>
2018-06-17 14:52:20 -0500

    changes

--- models/article.js
+++ models/article.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
-var ArticleSchema = new Schema({
+var NewsArticleSchema = new Schema({
     // `headline` is required and of type String
     headline: {
         type: String,
@@ -21,17 +21,17 @@ var ArticleSchema = new Schema({
         type: String,
         required: true
     },
-    // `note` is an object that stores a Note id
-    // The ref property links the ObjectId to the Note model
-    // This allows us to populate the Article with an associated Note
-    // note: {
-    //     type: Schema.Types.ObjectId,
-    //     ref: "Note"
-    // }
+    //`note` is an object that stores a Note id
+    //The ref property links the ObjectId to the Note model
+    //This allows us to populate the Article with an associated Note
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "NewsNote"
+    }
 });
 
 // This creates our model from the above schema, using mongoose's model method
-var Article = mongoose.model("Article", ArticleSchema);
+let NewsArticle = mongoose.model("NewsArticle", NewsArticleSchema);
 
 // Export the Article model
-module.exports = Article;
+module.exports = NewsArticle;
--- server.js
+++ server.js
@@ -84,11 +84,11 @@ app.get("/scrape", function (req, res) {
         });
 
          //  Create a new Article using the results object built from scraping
-        db.Article.create(results)
-            .then(function (dbArticle) {
+        db.NewsArticle.create(results)
+            .then(function (dbNewsArticle) {
                 // View the added result in the console
                 console.log("reached this point");
-                console.log(dbArticle);
+                console.log(dbNewsArticle);
             })
             .catch(function (err) {
                 // If an error occurred, send it to the client
@@ -104,10 +104,10 @@ app.get("/scrape", function (req, res) {
 // Route for getting all Articles from the db
 app.get("/articles", function (req, res) {
     // Grab every document in the Articles collection
-    db.Article.find({})
-        .then(function (dbArticle) {
+    db.NewsArticle.find({})
+        .then(function (dbNewsArticle) {
             // If we were able to successfully find Articles, send them back to the client
-            res.json(dbArticle);
+            res.json(dbNewsArticle);
         })
         .catch(function (err) {
             // If an error occurred, send it to the client
@@ -118,12 +118,12 @@ app.get("/articles", function (req, res) {
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function (req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-    db.Article.findOne({ _id: req.params.id })
+    db.NewsArticle.findOne({ _id: req.params.id })
         // ..and populate all of the notes associated with it
         .populate("note")
-        .then(function (dbArticle) {
+        .then(function (dbNewsArticle) {
             // If we were able to successfully find an Article with the given id, send it back to the client
-            res.json(dbArticle);
+            res.json(dbNewsArticle);
         })
         .catch(function (err) {
             // If an error occurred, send it to the client
@@ -134,14 +134,14 @@ app.get("/articles/:id", function (req, res) {
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function (req, res) {
     // Create a new note and pass the req.body to the entry
-    db.Note.create(req.body)
-        .then(function (dbNote) {
+    db.NewsNote.create(req.body)
+        .then(function (dbNewsNote) {
             // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
             // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
             // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
             return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
         })
-        .then(function (dbArticle) {
+        .then(function (dbNewsArticle) {
             // If we were able to successfully update an Article, send it back to the client
             res.json(dbArticle);
         })
