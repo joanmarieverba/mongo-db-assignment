e87dde7fa97c32c91c7455594921817ce63821a4
Joan Marie Verba <verba001@umn.edu>
2018-06-22 14:27:05 -0500

    cleanup

--- server.js
+++ server.js
@@ -123,23 +123,6 @@ app.get("/articles", function (req, res) {
         });
 });
 
-// Route for grabbing a specific Article by id, populate it with its note
-app.get("/articles/:id", function (req, res) {
-    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-    db.NewsArticle.findOne({ _id: req.params.id })
-        // ..and populate all of the notes associated with it
-        .populate("note")
-        .then(function (dbNewsArticle) {
-            // If we were able to successfully find an Article with the given id, send it back to the client
-            res.json(dbNewsArticle);
-        })
-        .catch(function (err) {
-            // If an error occurred, send it to the client
-            res.json(err);
-        });
-});
-
-
 // Save Article: change the value of saved
 app.post("/saved/:id", function (req, res) {
     
@@ -167,7 +150,6 @@ app.post("/articles/:id", function (req, res) {
         console.log("req.body ", req.body);
     console.log("req.params.id ", req.params.id);
     //need callback function in order for this to work
-    //db.NewsArticle.findOneAndUpdate(req.params.id, { note: req.body.note }, { new: true }, function (error, obj) {
     db.NewsArticle.findByIdAndUpdate(req.params.id, { $set: { note: req.body.note } }, { new: true }, function (error, obj) {
         if (error) next(error);
     });
