5d9176f1f50f1d6d35c9869e89618716376d8352
Joan Marie Verba <verba001@umn.edu>
2018-06-22 13:28:58 -0500

    progress:

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,16 +3,14 @@ $.getJSON("/articles", function (data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
         // Display the information on the page
-        
- //       $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].headline + "<br />" + data[i].url + "<br />" + data[i].summary + "<button data-id='" + data[i]._id + "'>SAVE</button> </p>");
- //       $("#articles").append(data[i].headline + "<button data-id='" + data[i]._id + "'>SAVE ARTICLE</button> <p>" + data[i].summary + "<p>");      
-//      $("#articles").append("<a href='" + data[i].url + "'>" + data[i].headline + "</a>"  + "<button data-id='" + data[i]._id + "'>SAVE ARTICLE</button> <p>" + data[i].summary + "<p>");
- //       $("#articles").append(`</div><a href=${data[i].url}>${data[i].headline}</a><button data-id=${data[i]._id}>SAVE ARTICLE</button><p>${data[i].summary}<p>`);
+  
         $("#articles").append(`<div class="panel panel-default">
         <div class="panel-heading">
-        <h3 class="panel-title"><a href=${data[i].url}>${data[i].headline}</a><button class="firstbtn" data-id=${data[i]._id}>SAVE ARTICLE</button><p></h3></div>
-            <div class="panel-body">${data[i].summary}<p></div></div>`);
-}
+        <h3 class="panel-title"><a href=${data[i].url}>${data[i].headline}</a>
+        <button class="firstbtn" data-id=${data[i]._id}>SAVE ARTICLE</button><p></h3></div>
+        <div class="panel-body">${data[i].summary}<p></div>
+        <div class="panel-body">${data[i].note}<p></div></div>`);
+    }
 });
 
 // click on scrape button
@@ -47,14 +45,7 @@ $(".btn-success").on("click", function () {
         // For each one
         for (var i = 0; i < data.length; i++) {
             // return to true when working
-            if (!data[i].saved) {
-                // $("#articles").append(`<div class="panel panel-default">
-                // <div class="panel-heading">
-                // <h3 class="panel-title"><a href=${data[i].url}>${data[i].headline}</a>
-                // <button class="deletebtn" data-id=${data[i]._id}>DELETE FROM SAVED</button></h3></div>
-                // <div class="panel-body">${data[i].summary}
-                // <button class="notebtn" data-id=${data[i]._id}>ARTICLE NOTE</button><p></div>
-                // <div class="panel-body">${data[i].note}<p></div></div>`);
+            if (data[i].saved) {
 
                 $("#articles").append(`<div class="panel panel-default">
                 <div class="panel-heading">
@@ -67,7 +58,7 @@ $(".btn-success").on("click", function () {
                 <div class="panel-body">
                 <form class="create-form inputbutton">
                 <div class="form-group">
-                <textarea id="submitNote" name="note" rows="1" cols="115">${data[i].note}</textarea>
+                <textarea id="submitNote" name="note" rows="1" cols="115"></textarea>
                 <button class="notebtn" data-id=${data[i]._id}>ARTICLE NOTE</button></form><p></div></div>`);
 
             };
@@ -77,15 +68,15 @@ $(".btn-success").on("click", function () {
 })
 
 // click on the save article buttom
-$(".firstbtn").on("click", function () {
+$("body").on("click", ".firstbtn", function () {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
-    // Save the id from the p tag
+    // Save the id
     let saveId = $(this).attr("data-id");
-
+    console.log ("clicked on save article");
     // Now make an ajax call for the Article
     $.ajax({
-        method: "GET",
+        method: "POST",
         url: "/saved/" + saveId
     })
         // With that done, nothing else to do
@@ -98,22 +89,26 @@ $(".firstbtn").on("click", function () {
         });
 });
 
-//  db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-
 // click on the article note button
-$(".notebtn").on("click", function () {
+$("body").on("click", ".notebtn", function () {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
     // Save the id from the p tag
     let noteId = $(this).attr("data-id");
-    let inputNote= $("#submitnote").val().trim();
+    let inputNote= $(this).prev().val().trim();
+    console.log("inputnote ", inputNote);
+    // store input note as an object
+    let objNote = { note: inputNote };
+    //clear input box
+    $("#submitnote").val("");
 
     // Now make an ajax call for the Article
     $.ajax({
-        method: "GET",
-        url: "/articles/" + noteId
+        method: "POST",
+        url: "/articles/" + noteId,
+        data: objNote
     })
-        // With that done, add the note information to the page???
+        // check note information
         .then(function (data) {
             console.log("notedata", data);
         })
@@ -125,68 +120,5 @@ $(".notebtn").on("click", function () {
 
 
 
-/////////////////////////////////////////////////////////////////////
-// Whenever someone clicks a p tag
-$(document).on("click", "p", function () {
-    // Empty the notes from the note section
-    $("#notes").empty();
-    // Save the id from the p tag
-    var thisId = $(this).attr("data-id");
-
-    // Now make an ajax call for the Article
-    $.ajax({
-        method: "GET",
-        url: "/articles/" + thisId
-    })
-        // With that done, add the note information to the page
-        .then(function (data) {
-            console.log(data);
-            // The title of the article
-            $("#notes").append("<h2>" + data.headline + "</h2>");
-            // An input to enter a new title
-            $("#notes").append("<input id='titleinput' name='title' >");
-            // A textarea to add a new note body
-            $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-            // A button to submit a new note, with the id of the article saved to it
-            $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-            // If there's a note in the article
-            if (data.note) {
-                // Place the title of the note in the title input
-                $("#titleinput").val(data.note.headline);
-                // Place the body of the note in the body textarea
-                $("#bodyinput").val(data.note.body);
-            }
-        });
-});
-
-// When you click the savenote button
-$(document).on("click", "#savenote", function () {
-    // Grab the id associated with the article from the submit button
-    var thisId = $(this).attr("data-id");
-
-    // Run a POST request to change the note, using what's entered in the inputs
-    $.ajax({
-        method: "POST",
-        url: "/articles/" + thisId,
-        data: {
-            // Value taken from title input
-            title: $("#titleinput").val(),
-            // Value taken from note textarea
-            body: $("#bodyinput").val()
-        }
-    })
-        // With that done
-        .then(function (data) {
-            // Log the response
-            console.log(data);
-            // Empty the notes section
-            $("#notes").empty();
-        });
-
-    // Also, remove the values entered in the input and textarea for note entry
-    $("#titleinput").val("");
-    $("#bodyinput").val("");
-});
 
 
--- server.js
+++ server.js
@@ -140,10 +140,14 @@ app.get("/articles/:id", function (req, res) {
 });
 
 
-// attempt to change the value of saved
+// Save Article: change the value of saved
 app.post("/saved/:id", function (req, res) {
-  
-    db.NewsArticle.findOneAndUpdate({ _id: req.params.id }, { saved: true },  { new: true } );
+    console.log("try to save");
+    //db.NewsArticle.findOneAndUpdate({ _id: req.params.id }, { saved: true },  { new: true } );
+
+    db.NewsArticle.findByIdAndUpdate(req.params.id, {$set : { saved: true }}, { new: true }, function (error, obj){
+        if (error ) next (error);
+    });
 
 });
 
@@ -152,23 +156,19 @@ app.post("/saved/:id", function (req, res) {
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function (req, res) {
     // Create a new note and pass the req.body to the entry
-    db.NewsNote.create(req.body)
-        .then(function (dbNewsNote) {
-            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. 
-            //Update the Article to be associated with the new Note
-            // { new: true } tells the query that we want it to return the updated User 
-            // -- it returns the original by default
-            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-        })
-        .then(function (dbNewsArticle) {
-            // If we were able to successfully update an Article, send it back to the client
-            res.json(dbNewsArticle);
-        })
-        .catch(function (err) {
-            // If an error occurred, send it to the client
-            res.json(err);
-        });
+        console.log("req.body ", req.body);
+    db.NewsArticle.findOneAndUpdate(req.params.id, { note: req.body.note }, { new: true }, function (error, obj) {
+        if (error) next(error);
+    });
+        // })
+        // .then(function (dbNewsArticle) {
+        //     // If we were able to successfully update an Article, send it back to the client
+        //     res.json(dbNewsArticle);
+        // })
+        // .catch(function (err) {
+        //     // If an error occurred, send it to the client
+        //     res.json(err);
+        // });
 });
 
 ////////////////////////////
